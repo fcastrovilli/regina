3f85c34c30982e8c354a1f39d8970a11e13a5ccc
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-04-06 21:33:27 +0200

    feat: colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	import Synth from '$lib/synth.svelte';
+	// import Synth from '$lib/synth.svelte';
+	import Colors from '$lib/colors.svelte';
+	import { all_notes } from '$lib/store';
 </script>
 
-<div class="p-4 w-full space-y-4">
+<!-- <div class="p-4 w-full space-y-4">
 	<Synth />
+</div> -->
+
+<div class="h-screen w-screen grid lg:grid-cols-4 grid-cols-2">
+	{#each $all_notes as voice}
+		<Colors {voice} />
+	{/each}
 </div>
