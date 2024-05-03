eb9d6b30b6895e8aef4966f76c695b2455770095
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-05-03 14:38:43 +0200

    fix: button to div

--- src/lib/images.svelte
+++ src/lib/images.svelte
@@ -28,93 +28,118 @@
 	<section class="p-0 m-0">
 		<div class="grid grid-cols-11">
 			<div class="col-span-7 h-full">
-				<button
+				<div>
+					<img
+						on:contextmenu={() => {
+							return false;
+						}}
+						on:pointerdown={() => attack(selected_scale.notes[0])}
+						on:pointerup={() => release(selected_scale.notes[0])}
+						on:pointerleave={() => release(selected_scale.notes[0])}
+						class="h-auto max-w-full"
+						src="./img/01_A.png"
+						alt="Sezione 1"
+					/>
+				</div>
+
+				<div class="flex flex-row">
+					<div>
+						<img
+							on:contextmenu={() => {
+								return false;
+							}}
+							on:pointerdown={() => attack(selected_scale.notes[3])}
+							on:pointerup={() => release(selected_scale.notes[3])}
+							on:pointerleave={() => release(selected_scale.notes[3])}
+							class="h-full w-auto"
+							src="./img/04_F.png"
+							alt="Sezione 4"
+						/>
+					</div>
+					<div>
+						<img
+							on:contextmenu={() => {
+								return false;
+							}}
+							on:pointerdown={() => attack(selected_scale.notes[4])}
+							on:pointerup={() => release(selected_scale.notes[4])}
+							on:pointerleave={() => release(selected_scale.notes[4])}
+							class="h-full w-auto"
+							src="./img/05_D.png"
+							alt="Sezione 5"
+						/>
+					</div>
+				</div>
+			</div>
+			<div class="grid col-span-4 grid-rows-1">
+				<div>
+					<img
+						on:contextmenu={() => {
+							return false;
+						}}
+						on:pointerdown={() => attack(selected_scale.notes[1])}
+						on:pointerup={() => release(selected_scale.notes[1])}
+						on:pointerleave={() => release(selected_scale.notes[1])}
+						class="h-auto max-w-full"
+						src="./img/02_B.png"
+						alt="Sezione 2"
+					/>
+				</div>
+				<div>
+					<img
+						on:contextmenu={() => {
+							return false;
+						}}
+						on:pointerdown={() => attack(selected_scale.notes[2])}
+						on:pointerup={() => release(selected_scale.notes[2])}
+						on:pointerleave={() => release(selected_scale.notes[2])}
+						class="h-auto max-w-full"
+						src="./img/03_G.png"
+						alt="Sezione 3"
+					/>
+				</div>
+				<div>
+					<img
+						on:contextmenu={() => {
+							return false;
+						}}
+						on:pointerdown={() => attack(selected_scale.notes[5])}
+						on:pointerup={() => release(selected_scale.notes[5])}
+						on:pointerleave={() => release(selected_scale.notes[5])}
+						class="h-auto max-w-full"
+						src="./img/06_C.png"
+						alt="Sezione 6"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="flex flex-row w-full">
+			<div>
+				<img
+					on:contextmenu={() => {
+						return false;
+					}}
+					on:pointerdown={() => attack(selected_scale.notes[6])}
+					on:pointerup={() => release(selected_scale.notes[6])}
+					on:pointerleave={() => release(selected_scale.notes[6])}
+					class="h-auto max-w-full"
+					src="./img/07_Fsharp.png"
+					alt="Sezione 7"
+				/>
+			</div>
+			<div>
+				<img
 					on:contextmenu={() => {
 						return false;
 					}}
 					on:pointerdown={() => attack(selected_scale.notes[0])}
 					on:pointerup={() => release(selected_scale.notes[0])}
 					on:pointerleave={() => release(selected_scale.notes[0])}
-				>
-					<img class="h-auto max-w-full" src="./img/01_A.png" alt="Sezione 1" />
-				</button>
-				<div class="flex flex-row">
-					<button
-						on:contextmenu={() => {
-							return false;
-						}}
-						on:pointerdown={() => attack(selected_scale.notes[3])}
-						on:pointerup={() => release(selected_scale.notes[3])}
-						on:pointerleave={() => release(selected_scale.notes[3])}
-					>
-						<img class="h-full w-auto" src="./img/04_F.png" alt="Sezione 4" />
-					</button>
-					<button
-						on:contextmenu={() => {
-							return false;
-						}}
-						on:pointerdown={() => attack(selected_scale.notes[4])}
-						on:pointerup={() => release(selected_scale.notes[4])}
-						on:pointerleave={() => release(selected_scale.notes[4])}
-					>
-						<img class="h-full w-auto" src="./img/05_D.png" alt="Sezione 5" />
-					</button>
-				</div>
+					class="h-auto max-w-full"
+					src="./img/08_E.png"
+					alt="Sezione 8"
+				/>
 			</div>
-			<div class="grid col-span-4 grid-rows-1">
-				<button
-					on:contextmenu={() => {
-						return false;
-					}}
-					on:pointerdown={() => attack(selected_scale.notes[1])}
-					on:pointerup={() => release(selected_scale.notes[1])}
-					on:pointerleave={() => release(selected_scale.notes[1])}
-				>
-					<img class="h-auto max-w-full" src="./img/02_B.png" alt="Sezione 2" />
-				</button>
-				<button
-					on:contextmenu={() => {
-						return false;
-					}}
-					on:pointerdown={() => attack(selected_scale.notes[2])}
-					on:pointerup={() => release(selected_scale.notes[2])}
-					on:pointerleave={() => release(selected_scale.notes[2])}
-				>
-					<img class="h-auto max-w-full" src="./img/03_G.png" alt="Sezione 3" />
-				</button>
-				<button
-					on:contextmenu={() => {
-						return false;
-					}}
-					on:pointerdown={() => attack(selected_scale.notes[5])}
-					on:pointerup={() => release(selected_scale.notes[5])}
-					on:pointerleave={() => release(selected_scale.notes[5])}
-				>
-					<img class="h-auto max-w-full" src="./img/06_C.png" alt="Sezione 6" />
-				</button>
-			</div>
-		</div>
-		<div class="flex flex-row w-full">
-			<button
-				on:contextmenu={() => {
-					return false;
-				}}
-				on:pointerdown={() => attack(selected_scale.notes[6])}
-				on:pointerup={() => release(selected_scale.notes[6])}
-				on:pointerleave={() => release(selected_scale.notes[6])}
-			>
-				<img class="h-auto max-w-full" src="./img/07_Fsharp.png" alt="Sezione 7" />
-			</button>
-			<button
-				on:contextmenu={() => {
-					return false;
-				}}
-				on:pointerdown={() => attack(selected_scale.notes[0])}
-				on:pointerup={() => release(selected_scale.notes[0])}
-				on:pointerleave={() => release(selected_scale.notes[0])}
-			>
-				<img class="h-auto max-w-full" src="./img/08_E.png" alt="Sezione 8" />
-			</button>
 		</div>
 	</section>
 </div>
